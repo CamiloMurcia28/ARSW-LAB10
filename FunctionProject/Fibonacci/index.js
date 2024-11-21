3d067920e5ed289e604457d1dbf567f626d9e115
CamiloMurcia28 <camilo281913@gmail.com>
2024-11-20 19:25:45 -0500

    Finalizado

--- FunctionProject/Fibonacci/index.js
+++ FunctionProject/Fibonacci/index.js
@@ -3,23 +3,29 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     let nth = req.body.nth
-    let nth_1 = bigInt.one;
-    let nth_2 = bigInt.zero;
-    let answer = bigInt.zero;
-
-    if (nth < 0)
-        throw 'must be greater than 0'
-    else if (nth === 0)
-        answer = nth_2
-    else if (nth === 1)
-        answer = nth_1
-    else {
-        for (var i = 0; i < nth - 1; i++) {
-            answer = nth_2.add(nth_1)
-            nth_2 = nth_1
-            nth_1 = answer
-        }
+    if (nth < 0){
+        context.res = {
+            status: 400,
+            body: "Input 'nth' must be a non-negative integer."
+        };
+        return;
     }
+    let mem = {};
+
+    function fibonacci(n){
+        if(n in mem)
+            return mem[n]
+        if(n == 0)
+            return bigInt.zero
+        if(n == 1)
+            return bigInt.one
+        let result = fibonacci(n-1).add(fibonacci(n-2));
+        mem[n] = result
+
+        return result
+    }
+
+    let res = fibonacci(nth);
 
     context.res = {
         body: answer.toString()
